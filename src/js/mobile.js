9379eb72a4c179991cd8544e29c477909043917b
tinfeek80 <jinl33@nycstudents.net>
2026-02-08 16:16:13 -0500

    fix resizing detecting height

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,44 +1,49 @@
-
 let Mobile_Width = 630;
 
 let Contacts_Section = document.querySelector(".Contacts_Section");
-let Information_Section = document.querySelector(".Information_Section")
+let Information_Section = document.querySelector(".Information_Section");
 let Return_To_Main_BTN = document.querySelector("#Return_To_Main_BTN");
 
 let Current_Width = window.innerWidth;
 
 function Switch_To_Mobile_Info() {
-    //console.log(Current_Width);
-    if (Current_Width > Mobile_Width){
-        console.log("Mobile Mode Not Allowed");
-        return;
-    }
-    console.log("Switching to Mobile Info");
-    Information_Section.scrollIntoView({behavior: "smooth"});
+  //console.log(Current_Width);
+  if (Current_Width > Mobile_Width) {
+    console.log("Mobile Mode Not Allowed");
+    return;
+  }
+  console.log("Switching to Mobile Info");
+  Information_Section.scrollIntoView({ behavior: "smooth" });
 }
 
 function Switch_To_Contacts() {
-    //console.log(Current_Width);
-    if (Current_Width > Mobile_Width){
-        console.log("Mobile Mode Not Allowed");
-        return;
-    }
-    console.log("Switching to Contacts");
-    Contacts_Section.scrollIntoView({behavior: "smooth"});
+  //console.log(Current_Width);
+  if (Current_Width > Mobile_Width) {
+    console.log("Mobile Mode Not Allowed");
+    return;
+  }
+  console.log("Switching to Contacts");
+  Contacts_Section.scrollIntoView({ behavior: "smooth" });
 }
 Return_To_Main_BTN.addEventListener("click", Switch_To_Contacts);
 
+let resizerTimer;
 
 function Update() {
-    Current_Width = window.innerWidth;
-    if (Current_Width <= Mobile_Width && Information_Section.classList.contains("Mobile") == false){
+  clearTimeout(resizerTimer);
+  resizerTimer = setTimeout(() => {
+    if (window.innerWidth != Current_Width) {
+      if (
+        Current_Width <= Mobile_Width &&
+        Information_Section.classList.contains("Mobile") == false
+      ) {
         Information_Section.classList.add("Mobile");
-        
-    } else {
+      } else {
         Information_Section.classList.remove("Mobile");
         Switch_To_Contacts();
+      }
     }
-    
+  }, 200);
 }
 
 window.addEventListener("resize", Update);
